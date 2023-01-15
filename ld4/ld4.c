fadd3859c7c9cf53fae36c562c7d148ef7414446
Whymanki <112925770+Whymanki@users.noreply.github.com>
2023-01-15 15:54:01 +0200

    Create ld4.c

---
+++ ld4/ld4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+double f(double x) {
+    // Funkcija f(x)
+    return x*x;
+}
+
+double rectangleRule(double a, double b, double h) {
+    double area = 0;
+    for (double x = a; x < b; x += h) {
+        area += f(x)*h;
+    }
+    return area;
+}
+
+double trapezoidalRule(double a, double b, double h) {
+    double area = 0;
+    for (double x = a; x < b; x += h) {
+        area += (f(x) + f(x+h))*h/2;
+    }
+    return area;
+}
+
+double simpsonsRule(double a, double b, double h) {
+    double area = 0;
+    for (double x = a; x < b; x += h) {
+        area += (f(x) + 4*f(x+h/2) + f(x+h))*h/6;
+    }
+    return area;
+}
+
+int main() {
+    double a, b, precision;
+    printf("Ievadi vērtību a: ");
+    scanf("%lf", &a);
+    printf("Ievadi vērtību b: ");
+    scanf("%lf", &b);
+    printf("Ievadi precizitāti: ");
+    scanf("%lf", &precision);
+
+    double h = (b - a) * precision;
+    printf("Taisnastūra likuma vērtība: %lf\n", rectangleRule(a, b, h));
+    printf("Trapeces likuma vērtība: %lf\n", trapezoidalRule(a, b, h));
+    printf("Simposna likuma vērtība: %lf\n", simpsonsRule(a, b, h));
+    return 0;
+}
